c8d4a13b73a71a30b3083636a64ff23dc052a718
Santiago Smith <sssilvar@unal.edu.co>
2018-02-12 14:56:00 -0500

    Thickness classification (AUC=0.55)

--- bin/conversion_detect_vol_tk.py
+++ bin/conversion_detect_vol_tk.py
@@ -2,19 +2,24 @@ from __future__ import print_function
 
 import os
 import json
+import numpy as np
 import pandas as pd
 import lib.Freesurfer as Fs
 import matplotlib.pyplot as plt
 
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import Lasso
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, roc_curve, roc_auc_score
 
 # Define params
 root = os.path.join(os.getcwd(), '..')
 params_file = os.path.join(root, 'param', 'params.json')
+features_file = 'thickness_features.csv'
+features_file = os.path.join(root, 'features', features_file)
 
 # Load params
 with open(params_file) as json_file:
@@ -25,16 +30,16 @@ with open(params_file) as json_file:
 
 # Load dataset data into a DataFrame: df
 df = pd.read_csv(os.path.join(root, data_file))
+df = df.sort_values('folder')
 
 # Define a subject
 subjects_zipped = zip(df['folder'], df['dx_group'], df['target'])
 
 # Create a FS object
-Fs = Fs.Freesurfer(dataset_folder, df)
+fs_obj = Fs.Freesurfer(dataset_folder, df)
 
 # Define a DataFrame for features extracted
-cols = ['region_id', 'region_name', 'mean', 'std', 'hemisphere', 'subject_id', 'target_name', 'target']
-features_df = pd.DataFrame(columns=cols)
+features_df = pd.DataFrame()
 
 # Define an error dict
 error = {
@@ -44,7 +49,7 @@ error = {
 }
 
 # Check for features file 'features.csv'
-if not os.path.exists(os.path.join(os.getcwd(), 'features.csv')):
+if not os.path.exists(features_file):
 
     # Start extracting features
     for i, (subject, dx_group, label) in enumerate(subjects_zipped):
@@ -54,21 +59,18 @@ if not os.path.exists(os.path.join(os.getcwd(), 'features.csv')):
             print(subject_folder)
 
             # Load labels
-            tk_stats_lh, _ = Fs.read_thickness(subject, 'lh')
-            tk_stats_rh, _ = Fs.read_thickness(subject, 'rh')
+            tk_stats_df, _ = fs_obj.read_thickness(subject)
 
             # Merge Hemispheres info
             print('[  OK  ] Concatenating DataFrames')
-            frames = [tk_stats_lh, tk_stats_rh]
-            tk_stats_df = pd.concat(frames)
 
             # Add extra info
             print('[  OK  ] Processing data')
-            tk_stats_df['hemi_and_reg'] = tk_stats_df['hemisphere'] + '_' + tk_stats_df['region_name']
             tk_stats_df['subject_id'] = subject
             tk_stats_df['target_name'] = dx_group
             tk_stats_df['target'] = label
 
+            # features_df.columns = tk_stats_df.columns
             features_df = features_df.append(tk_stats_df)
         except IOError as e:
             print('[  ERROR  ] it seems like subject %s does not exist' % subject)
@@ -81,37 +83,83 @@ if not os.path.exists(os.path.join(os.getcwd(), 'features.csv')):
     print('List of subjects with errors: \n', error['subjects'])
 
     # Save results to a csv file
-    features_df.to_csv('features.csv')
+    features_df = features_df.reset_index()
+    features_df.to_csv(features_file)
 
 
-# # Start Classification
-# print('\n\n[  OK  ] STARTING CLASSIFICATION')
-# features_df = pd.read_csv('features.csv')
-#
-# # Extract features (mean and std)
-# df_mean = features_df.pivot(index='subject_id', columns='hemi_and_reg', values='mean')
-# df_std = features_df.pivot(index='subject_id', columns='hemi_and_reg', values='std')
-#
-# df_mean['target'] = df_mean.apply(lambda x: print(x.index))
-#
-# # Define X and y
-# X = features_df[['mean', 'std']].values
-# y = features_df['target'].values
-#
-# # Split data
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7, random_state=21, stratify=y)
-#
-# # Select relevant features: coef
-# # lasso = Lasso(alpha=0.4, normalize=True)
-# lasso = Lasso(alpha=0.4)
-# lasso.fit(X, y)
-#
-# lasso_coef = lasso.coef_
-# print(lasso_coef)
-#
-# # Plot features
-# plt.figure()
-# plt.plot(range(X.shape[1]), lasso_coef)
-# plt.xticks(range(X.shape[1]), features_df['region_name'], rotation=60)
-# plt.margins(0.02)
-# plt.show(block=False)
+# Start Classification
+print('\n\n[  OK  ] STARTING CLASSIFICATION')
+features_df = pd.read_csv(features_file)
+features_df = features_df.drop(['Unnamed: 0', 'index'], axis=1)
+
+
+# Define X and y
+X_df = features_df.drop(['subject_id', 'target_name', 'target'], axis=1)
+X = X_df.values
+y = features_df['target'].values
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7, random_state=21, stratify=y)
+
+
+# # Start Classifying: SVM-RBF
+lasso = Lasso(alpha=0.020408163265306121)
+
+lasso.fit(X_train, y_train)
+
+lasso_coef = lasso.coef_
+print(lasso_coef)
+
+# Plot features
+plt.style.use('ggplot')
+plt.figure()
+plt.plot(range(X.shape[1]), lasso_coef)
+plt.xticks(range(X.shape[1]), X_df.columns, rotation=30)
+plt.margins(0.02)
+
+selected_features_cols = X_df.columns[lasso_coef != 0]
+print(selected_features_cols)
+
+# Define a new X (feature selection)
+X = X_df[selected_features_cols]
+
+# Split data (again)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7, random_state=21, stratify=y)
+
+# Start classifying
+print('[INFO]: Starting Pipeline...')
+pipeline = Pipeline([
+    ('scaler', StandardScaler()),
+    ('svm', SVC(kernel='rbf', probability=True))
+])
+
+param_grid = {
+    'svm__C': np.logspace(-3, 2, 20),
+    'svm__gamma': np.logspace(-3, 2, 20)
+}
+
+pipeline = GridSearchCV(pipeline, param_grid)
+
+
+print('[INFO]: Fitting model...')
+pipeline.fit(X_train, y_train)
+y_pred = pipeline.predict(X_test)
+
+print('Classification report: \n {}'.format(classification_report(y_test, y_pred)))
+print('Score: {}'.format(pipeline.score(X_test, y_test)))
+print('Best Params: {}'.format(pipeline.best_params_))
+
+y_pred_proba = pipeline.predict_proba(X_test)[:,1]
+
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_proba)
+auc = roc_auc_score(y_test, y_pred_proba)
+
+plt.figure()
+plt.plot([0, 1], [0, 1], 'k--')
+plt.plot(fpr, tpr)
+plt.legend(['AUC = ' + str(auc)])
+plt.xlabel('False positive rate')
+plt.ylabel('True positive rate')
+plt.title('ROC curve')
+
+plt.show(block=False)
--- lib/Freesurfer.py
+++ lib/Freesurfer.py
@@ -6,38 +6,59 @@ import nibabel.freesurfer as nbfs
 
 class Freesurfer:
 
-    columns = ['region_id', 'region_name', 'mean', 'std', 'hemisphere']
+    # columns = ['region_id', 'region_name', 'mean', 'std', 'hemisphere']
 
     def __init__(self, dataset_folder, df):
         self.dataset_folder = dataset_folder
         self.df = df
 
-    def read_thickness(self, subject_id, hemi):
+    def read_thickness(self, subject_id):
         # Set subject folder
         subject_folder = os.path.join(self.dataset_folder, subject_id)
         print('[  FS  ] Loading subject %s' % subject_folder)
 
         # Load labels
-        labels_id, ctab, labels = nbfs.read_annot(os.path.join(subject_folder, 'label', hemi + '.aparc.annot'))
-        labels_id[labels_id == -1] = 0  # -1 Correction
-        thickness = nbfs.read_morph_data(os.path.join(subject_folder, 'surf', hemi + '.thickness'))
+        labels_id_lh, ctab_lh, labels_lh = nbfs.read_annot(os.path.join(subject_folder, 'label', 'lh' + '.aparc.annot'))
+        labels_id_lh[labels_id_lh == -1] = 0  # -1 Correction
+        thickness_lh = nbfs.read_morph_data(os.path.join(subject_folder, 'surf', 'lh' + '.thickness'))
 
-        tk_stats = pd.DataFrame(columns=self.columns)
-        for i, label in enumerate(labels):
-            if thickness[labels_id == i].any():
-                tk_mean = np.mean(np.nan_to_num(thickness[labels_id == i]))
-                tk_std = np.std(np.nan_to_num(thickness[labels_id == i]))
+        labels_id_rh, ctab_rh, labels_rh = nbfs.read_annot(os.path.join(subject_folder, 'label', 'rh' + '.aparc.annot'))
+        labels_id_rh[labels_id_rh == -1] = 0  # -1 Correction
+        thickness_rh = nbfs.read_morph_data(os.path.join(subject_folder, 'surf', 'rh' + '.thickness'))
+
+
+        columns = map(lambda x: 'lh_' + x + '_mean', labels_lh) + map(lambda x: 'lh_' + x + '_std', labels_lh) + \
+                  map(lambda x: 'rh_' + x + '_mean', labels_rh) + map(lambda x: 'rh_' + x + '_std', labels_rh)
+        tk_stats = pd.DataFrame(columns=columns)
+
+        # Left hemisphere
+        tk_mean_lh = []
+        tk_std_lh = []
+        tk_mean_rh = []
+        tk_std_rh = []
+
+        for i, label in enumerate(labels_lh):
+            if thickness_lh[labels_id_lh == i].any():
+                tk_mean_lh.append(np.mean(np.nan_to_num(thickness_lh[labels_id_lh == i])))
+                tk_std_lh.append(np.std(np.nan_to_num(thickness_lh[labels_id_lh == i])))
             else:
-                tk_mean = 0
-                tk_std = 0
+                tk_mean_lh.append(0)
+                tk_std_lh.append(0)
 
-            tk_stats.loc[i] = [i, labels[i], tk_mean, tk_std, hemi]
+        for i, label in enumerate(labels_lh):
+            if thickness_lh[labels_id_lh == i].any():
+                tk_mean_rh.append(np.mean(np.nan_to_num(thickness_rh[labels_id_rh == i])))
+                tk_std_rh.append(np.std(np.nan_to_num(thickness_rh[labels_id_rh == i])))
+            else:
+                tk_mean_rh.append(0)
+                tk_std_rh.append(0)
+
+        tk_stats.loc[0] = tk_mean_lh + tk_std_lh + tk_mean_rh + tk_std_rh
 
         morph_data = {
-            # 'tk_stats_mean': tk_stats.pivot(index=i, columns=labels, values='mean'),
-            # 'tk_stats_std': tk_stats.pivot(index=i, columns=labels, values='std'),
-            'ctab': ctab,
-            'labels': labels
+            'labels_id': [labels_id_lh, labels_id_rh],
+            'ctab': [ctab_lh, ctab_rh],
+            'labels': [labels_lh, labels_rh]
         }
 
         return tk_stats, morph_data
