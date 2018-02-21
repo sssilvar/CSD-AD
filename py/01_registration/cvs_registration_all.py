0434677c02cb2f633b346bd8ba82a3abd1779ea9
sssilvar <sssilvar@unal.edu.co>
2018-02-21 15:52:59 -0500

    cvs registration - MMulticore

--- py/01_registration/cvs_registration_all.py
+++ py/01_registration/cvs_registration_all.py
@@ -33,6 +33,6 @@ if __name__ == '__main__':
 
     # Start multicore
     print('[  OK  ] Starting pool')
-    pool = Pool(4)
+    pool = Pool(10)
     pool.map(register_subject, df['folder'])
     pool.close()
