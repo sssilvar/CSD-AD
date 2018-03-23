9c04ac28bccad1051082d57b913df43c6634750c
Santiago Smith <sssilvar@unal.edu.co>
2018-03-23 15:16:38 -0500

    Missed: Whole dataset analysis

--- lib/visualization.py
+++ lib/visualization.py
@@ -3,9 +3,9 @@ import numpy as np
 
 
 def show_mri(vol, slice_xyz=(128, 128, 128)):
-    img_x = vol[:, :, slice_xyz[0]].reshape((256, 256))
-    img_y = vol[:, slice_xyz[1], :].reshape((256, 256))
-    img_z = vol[slice_xyz[2], :, :].reshape((256, 256))
+    img_x = vol[:, :, slice_xyz[0]]
+    img_y = vol[:, slice_xyz[1], :]
+    img_z = vol[slice_xyz[2], :, :]
 
     plt.figure()
     plt.subplot(1, 3, 1)
--- test/10_center_of_mass.py
+++ test/10_center_of_mass.py
@@ -1,10 +1,14 @@
 import os
 
 import numpy as np
+import nibabel as nb
+import matplotlib.pyplot as plt
 from scipy.ndimage.measurements import center_of_mass
 
+from lib.visualization import show_mri
+
 # Define root
-root = os.path.dirname(os.getcwd())
+root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 
 def get_centroid(mat):
@@ -19,6 +23,23 @@ def get_centroid(mat):
 
 
 if __name__ == '__main__':
-    x = np.ones([256, 256, 256])
+    # Load subject segmentation file
+    aseg_file = os.path.join(root, 'test', 'test_data', 'fsaverage.mgz')
+    aseg = nb.load(aseg_file)
+    data = aseg.get_data()
 
-    print(a)
+    # Consider all the brain volume and exclude non brain regions
+    mask = data > 0
+    print(mask.shape)
+
+    # Get center of mass (centroid)
+    a = get_centroid(mask)
+    print('Centroid', a)
+    print('Data shape {}'.format(a.shape))
+    a = get_centroid(data)
+    print('Centroid', a)
+    print('Data shape {}'.format(a.shape))
+
+    # Plot the result
+    show_mri(mask)
+    plt.show()
