88c46f12e7813a59a2d7fdd0c221d6a4a111a24d
Santiago Smith <sssilvar@unal.edu.co>
2018-03-28 05:58:48 -0700

    Multi-scale analysis: more scales added (corrected solid_cone center)

--- py/04_mapping_sphere/solid_angle_sphere_to_plane.py
+++ py/04_mapping_sphere/solid_angle_sphere_to_plane.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     for n_scale, scale in enumerate(scales):
         # Crete a solid angle from a scale: sa
         r_min, r_max = scale
-        sa = solid_cone(radius=(r_min, r_max))
+        sa = solid_cone(radius=(r_min, r_max), center=centroid)
 
         # Start go over the whole sphere (x_angle: [0, pi] and z_angle [-pi, pi])
         img_2d = np.zeros([360, 180])
@@ -52,7 +52,8 @@ if __name__ == '__main__':
                 img_masked = vol_sub * solid_ang_mask
 
                 img_2d[i, j] = np.nan_to_num(img_masked.sum() / solid_ang_mask.sum())
-            print('[ SA ] Scale: %d %s | Point (%d, %d) of (360/180) | Value: %f' % (n_scale + 1, scale, i, j, img_2d[i, j]))
+                print('[ SA ] Scale: %d %s Ang: %s | Point (%d, %d) of (360/180) | Value: %f' %
+                      (n_scale + 1, scale, (x_angle, z_angle), i, j, img_2d[i, j]))
 
         img_filename = os.path.join(root, 'output', '%d_to_%d_solid_angle_to_sphere.png' % (r_min, r_max))
         plt.imsave(img_filename, img_2d, cmap='gray')
--- test/solid_angle_mask_generation/solid_angle_extract_volume.py
+++ test/solid_angle_mask_generation/solid_angle_extract_volume.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     aseg = nb.load(aseg_filename).get_data()
 
     # Define radius and center
-    radius = (33, 66)
+    radius = (0, 25)
     centroid = tuple(get_centroid(aseg > 0))
     print('[  OK  ] Centroid = {}'.format(centroid))
 
@@ -35,10 +35,13 @@ if __name__ == '__main__':
     mask_sub, _ = extract_sub_volume(mask, radius=radius, centroid=centroid)
 
     mask_sub = rotate_vol(mask_sub, angles=(45, 0, 0))
+    solid_ang_mask = rotate_vol(mask_sub, angles=(0, 0, -180))
 
     # Mask sub-sampled volume
-    vol_masked_sub = vol_sub * mask_sub
+    vol_masked_sub = vol_sub * solid_ang_mask
+    mean_int = np.nan_to_num(vol_masked_sub.sum() / solid_ang_mask.sum())
 
+    print(mean_int)
     show_mri(vol_masked_sub, slice_xyz=center)
     plt.suptitle('Original Image')
     plt.show()
