ae81b899f96fe86a37c6e6a2f777adcc36d0e0ee
Santiago Smith <sssilvar@unal.edu.co>
2018-04-02 23:47:45 -0500

    Wavelet decomposition

--- test/wavelets/decomose_image_2.py
+++ test/wavelets/decomose_image_2.py
@@ -21,7 +21,7 @@ def plot_wavelet_decomposition(image, level=3, wavelet='haar'):
     coeffs = pywt.wavedec2(image, wavelet=wavelet, level=level)
     for i, (cH, cV, cD) in enumerate(coeffs[1:]):
         if i == 0:
-            cAcH = np.concatenate((coeffs[0] / np.max(coeffs[0]), cH), axis=1)
+            cAcH = np.concatenate((coeffs[0] * 0.025, cH), axis=1)
             cVcD = np.concatenate((cV, cD), axis=1)
             plot_image = np.concatenate((cAcH, cVcD), axis=0)
         else:
@@ -33,14 +33,36 @@ def plot_wavelet_decomposition(image, level=3, wavelet='haar'):
     plt.grid(False)
     plt.imshow(abs(plot_image), cmap='gray')
     plt.title('Wavelet (%s) decomposition - Level %d' % (wavelet, level))
+    plt.axis('off')
     # mng = plt.get_current_fig_manager()
     # mng.full_screen_toggle()
 
 
 if __name__ == '__main__':
-    img_filename = os.path.join(root, 'test', 'test_data', 'sphere_mapped', '0729',
-                                'intensity_0_to_25_solid_angle_to_sphere.raw')
-    img = np.fromfile(img_filename, dtype=np.float).reshape([360, 180]).T
+    # MCIc
+    # img_filename = os.path.join(root, 'test', 'test_data', 'sphere_mapped', '0729',
+    #                             'gradient_25_to_50_solid_angle_to_sphere.raw')
 
-    plot_wavelet_decomposition(img, level=2, wavelet='haar')
-    plt.show()
+    img_dir = "Z:/Users/Smith/Downloads/temp/Thesis/results/4_scales/" \
+                   "MCIc/002_S_1070"
+
+    # # MCInc
+    # img_filename = "Z:/Users/Smith/Downloads/temp/Thesis/results/4_scales/" \
+    #                "MCInc/005_S_0448/gradient_0_to_25_solid_angle_to_sphere.raw"
+
+    images = [
+        'gradient_0_to_25_solid_angle_to_sphere.raw',
+        'gradient_25_to_50_solid_angle_to_sphere.raw',
+        'gradient_50_to_75_solid_angle_to_sphere.raw',
+        'gradient_75_to_100_solid_angle_to_sphere.raw'
+    ]
+
+    for image in images:
+        img_filename = os.path.join(img_dir, image)
+
+        img = np.fromfile(img_filename, dtype=np.float).reshape([360, 180]).T
+        out_img = os.path.splitext(img_filename)[0] + '_wave_dec.png'
+
+        plot_wavelet_decomposition(img, level=1, wavelet='haar')
+        plt.savefig(out_img, bbox_inches='tight')
+        # plt.show()
