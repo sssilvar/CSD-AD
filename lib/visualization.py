f7f60924e7d0b0ddee0f5bd7108c6633c75e9cf0
Santiago Smith <sssilvar@unal.edu.co>
2018-04-08 09:30:31 -0500

    Multiscale visualization

--- lib/visualization.py
+++ lib/visualization.py
@@ -1,8 +1,17 @@
+import os
+
+import matplotlib.font_manager as fm
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def show_mri(vol, slice_xyz=(128, 128, 128)):
+def show_mri(vol, slice_xyz=(128, 128, 128), axis='off', grid='off'):
+    prop = fm.FontProperties(
+        fname=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'fonts', 'Fira_Sans', 'FiraSans-Bold.ttf')
+    )
+    fontsize = 18
+    color = (0.09, 0.11, 0.18)
+
     img_x = vol[:, :, slice_xyz[0]]
     img_y = vol[:, slice_xyz[1], :]
     img_z = vol[slice_xyz[2], :, :]
@@ -12,13 +21,23 @@ def show_mri(vol, slice_xyz=(128, 128, 128)):
     plt.imshow(img_x, cmap='gray')
     plt.xlabel('X')
     plt.ylabel('Y')
+    plt.title('XY Plane', fontproperties=prop, size=fontsize, color=color)
+    plt.axis(axis)
+    plt.grid(grid)
 
     plt.subplot(1, 3, 2)
     plt.imshow(img_y, cmap='gray')
     plt.xlabel('X')
     plt.ylabel('Z')
+    plt.title('XZ Plane', fontproperties=prop, size=fontsize, color=color)
+    plt.axis(axis)
+    plt.grid(grid)
 
     plt.subplot(1, 3, 3)
     plt.imshow(img_z, cmap='gray')
     plt.xlabel('Y')
     plt.ylabel('Z')
+    plt.title('YZ Plane', fontproperties=prop, size=fontsize, color=color)
+    plt.axis(axis)
+    plt.grid(grid)
+
--- test/solid_angle_mask_generation/solid_angle_extract_volume.py
+++ test/solid_angle_mask_generation/solid_angle_extract_volume.py
@@ -13,6 +13,8 @@ from lib.transformations import rotate_vol
 from lib.visualization import show_mri
 from lib.geometry import get_centroid, extract_sub_volume
 
+plt.style.use('ggplot')
+
 if __name__ == '__main__':
     # Define file names
     vol_filename = os.path.join(root, 'test', 'test_data', '941_S_1363.mgz')
@@ -23,25 +25,34 @@ if __name__ == '__main__':
     aseg = nb.load(aseg_filename).get_data()
 
     # Define radius and center
-    radius = (0, 25)
+    # radius = (0, 25)
+    scales = [
+        (0, 25),
+        (25, 50),
+        (50, 75),
+        (75, 100)
+    ]
     centroid = tuple(get_centroid(aseg > 0))
     print('[  OK  ] Centroid = {}'.format(centroid))
 
-    # Create a binary mask (cone between scales)
-    mask = solid_cone(radius=radius, center=centroid)
+    for radius in scales:
+        # Create a binary mask (cone between scales)
+        mask = sphere(radius=radius, center=centroid)
 
-    # Subsample the whole volume and the mask
-    vol_sub, center = extract_sub_volume(vol, radius=radius, centroid=centroid)
-    mask_sub, _ = extract_sub_volume(mask, radius=radius, centroid=centroid)
+        # Subsample the whole volume and the mask
+        vol_sub, center = extract_sub_volume(vol, radius=radius, centroid=centroid)
+        mask_sub, _ = extract_sub_volume(mask, radius=radius, centroid=centroid)
 
-    mask_sub = rotate_vol(mask_sub, angles=(45, 0, 0))
-    solid_ang_mask = rotate_vol(mask_sub, angles=(0, 0, -180))
+        mask_sub = rotate_vol(mask_sub, angles=(45, 0, 0))
+        solid_ang_mask = rotate_vol(mask_sub, angles=(0, 0, -180))
 
-    # Mask sub-sampled volume
-    vol_masked_sub = vol_sub * solid_ang_mask
-    mean_int = np.nan_to_num(vol_masked_sub.sum() / solid_ang_mask.sum())
+        # Mask sub-sampled volume
+        vol_masked_sub = vol_sub * solid_ang_mask
+        mean_int = np.nan_to_num(vol_masked_sub.sum() / solid_ang_mask.sum())
 
-    print(mean_int)
-    show_mri(vol_masked_sub, slice_xyz=center)
-    plt.suptitle('Original Image')
-    plt.show()
+        print(mean_int)
+        image_out_filename = os.path.join(root, 'output', 'intensity_%d_to_%d_three_views.png' % radius)
+
+        show_mri(vol_masked_sub, slice_xyz=center)
+        plt.savefig(image_out_filename, cmap='gray', bbox_inches='tight')
+        # plt.show()
