016453a3680101ed14f6fbec7459c995b9832a35
Santiago Smith <sssilvar@unal.edu.co>
2018-04-16 16:48:03 -0500

    Curvelet decomposition circular plot library added

--- py/05_sparse_representation/curvelet_decomposition.py
+++ py/05_sparse_representation/curvelet_decomposition.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
 
     file_results = os.path.join(root, 'output', 'curve_dec_test.json')
     with open(file_results, 'w') as fp:
-        json.dump(f_dict, fp)
+        json.dump(f_dict, fp, sort_keys=True, indent=4)
 
     script = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'plot_results_python3.py')
     os.system('python %s %s %d %d' % (script, file_results, n_scales, n_angles))
