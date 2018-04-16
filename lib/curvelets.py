374561ae89cf85afb5aa62d09d8a66bcbd3d7518
Santiago Smith <sssilvar@unal.edu.co>
2018-04-16 17:20:54 -0500

    Curvelet decomposition circular plot library added

--- lib/curvelets.py
+++ lib/curvelets.py
@@ -15,19 +15,19 @@ def curvelet_plot(scales, angles, values):
 
         sub_bands = []
         if scale == 0:
-            sub_bands.append(gr(val[0]))
+            sub_bands.append(gr(float(val[0])))
             group_size = [1]
         elif scale == 1:
             for angle in range(0, angles):
-                sub_bands.append(gr(val[angle]))
+                sub_bands.append(gr(float(val[angle])))
             group_size = list(np.ones(angles))
         elif scale % 2 == 0:
             for angle in range(0, int(scale * angles)):
-                sub_bands.append(gr(val[angle]))
+                sub_bands.append(gr(float(val[angle])))
             group_size = list(np.ones(angles))
         elif scale % 2 != 0:
             for angle in range(0, int((scale - 1) * angles)):
-                sub_bands.append(gr(val[angle]))
+                sub_bands.append(gr(float(val[angle])))
             group_size = list(np.ones(angles))
         else:
             sub_bands = group_size = None
