e3aa557b2601cf5fc7aa52a15e83f3bd6effe4d7
Santiago Smith <sssilvar@unal.edu.co>
2018-04-19 16:25:50 -0500

    Curvelet decomposition circular plot library added

--- py/05_sparse_representation/plot_results_python3.py
+++ py/05_sparse_representation/plot_results_python3.py
@@ -18,10 +18,11 @@ if __name__ == '__main__':
     # Set output folder [curvelets/png]
     # Here, the visual results will be saved
     output_folder = os.path.join(os.path.dirname(filename), 'png')
-    print('[  OK  ] Visual folder output')
+    filename_output = os.path.join(output_folder, os.path.basename(filename)[:-4] + '.png')
 
-    print('Loading %s...' % filename)
+    print('Loading %s...\n\n' % filename)
     data = np.load(filename).item()
 
+    print('Saving figure at: ' + filename_output)
     curvelet_plot(n_scales, n_angles, data)
     plt.show()
