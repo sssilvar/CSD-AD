1f60104b8f243436bc5b83e2dd0bca5a3e8949ff
Santiago S. Silva R <sssilvar@unal.edu.co>
2018-06-07 06:58:58 +0200

    Vertex-wise analysis

--- test/shape_analysis/vertex_wise_shape_analysis.py
+++ test/shape_analysis/vertex_wise_shape_analysis.py
@@ -3,23 +3,50 @@ import os
 import numpy as np
 import pandas as pd
 from scipy.stats import ttest_ind
+import matplotlib.pyplot as plt
 
 up = os.path.dirname
 root = up(up(up(os.path.realpath(__file__))))
 
+plt.style.use('ggplot')
+
 if __name__ == '__main__':
     csv_data = os.path.join(root, 'param', 'data_df.csv')
-    csv_thickness = '/home/sssilvar/Documents/data/cortical_shape_analysis/groupfile_thick_2e-4.csv'
+    csv_features = '/home/sssilvar/Documents/data/cortical_shape_analysis/groupfile_thick_2e-4.csv'
+    # csv_features = '/home/sssilvar/Documents/data/cortical_shape_analysis/output/groupfile_thick.csv'
 
     # Load data
     df_dataset = pd.read_csv(csv_data)
-    df_thickness = pd.read_csv(csv_thickness)
+    df_thickness = pd.read_csv(csv_features, index_col=0)
 
-    # TEST
-    data_a = np.random.normal(10, 10, 50)
-    data_b = np.random.normal(18, 10, 55)
+    mci_c_subjects = df_dataset.loc[df_dataset['target_categories'] == 'MCIc']['folder']
+    mci_nc_subjects = df_dataset.loc[df_dataset['target_categories'] == 'MCInc']['folder']
 
-    # Start t-test
-    t, p = ttest_ind(data_a, data_b)
+    df_mci_c = df_thickness.loc[df_thickness.index.isin(mci_c_subjects)]
+    df_mci_nc = df_thickness.loc[df_thickness.index.isin(mci_nc_subjects)]
 
-    print(p)
+    t_stat = []
+    p_val = []
+
+    for column in df_mci_c:
+        t, p = ttest_ind(df_mci_nc[column], df_mci_c[column])
+
+        t_stat.append(t)
+        p_val.append(p)
+
+    print('[  INFO  ] Saving results in ./output')
+    np.array(t_stat).tofile(os.path.join(root, 'output', 't_stat_' + os.path.basename(csv_features[:-4]) + '.raw'))
+    np.array(p_val).tofile(os.path.join(root, 'output', 'p_value_' + os.path.basename(csv_features[:-4]) + '.raw'))
+
+    print('DONE!')
+
+    # f, ar = plt.subplots(2, 1, sharex=True)
+    # ar[0].plot(p_val)
+    # ar[0].plot(np.ones_like(p_val) * 0.05, 'b--')
+    # ar[0].legend(['p-value', '0.05'])
+    # ar[0].set_yscale('log')
+    #
+    # ar[1].plot(t_stat)
+    # ar[1].legend(['t-statistic'])
+    #
+    # plt.show()
