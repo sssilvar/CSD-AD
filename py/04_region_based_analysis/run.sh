4be0e5cf30ca35f3e7014e13d1cdd81fc2d6c921
SILVA RINCON Santiago Smith <santiago-smith.silva-rincon@inria.fr>
2018-06-14 06:10:27 +0200

    A regional approach.

--- py/04_region_based_analysis/run.sh
+++ py/04_region_based_analysis/run.sh
@@ -4,9 +4,9 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Run feature extraction
-eval "python3 "${CURRENT_DIR}"/region_feature_extraction.py"
+eval "python "${CURRENT_DIR}"/region_feature_extraction.py"
 
 # Run Classification
-cmd="python3 "${CURRENT_DIR}"/relevant_regions.py > "${CURRENT_DIR}"/../../output/gmm_region_based.log"
+cmd="python "${CURRENT_DIR}"/relevant_regions.py > "${CURRENT_DIR}"/../../output/gmm_region_based.log"
 echo -e "\n\n[  INFO  ] Running classification stage"
 eval ${cmd}
