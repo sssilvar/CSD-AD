d478219b9356881c874257dff37ac86313d2b536
Santiago Smith <sssilvar@unal.edu.co>
2019-01-19 08:53:04 -0500

    Parse Configuration

--- py/04_mapping_sphere/solid_angle_sphere_to_plane.py
+++ py/04_mapping_sphere/solid_angle_sphere_to_plane.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from configparser import ConfigParser
-from os.path import join, dirname, realpath
+from os.path import join, dirname, realpath, isfile
 
 import numpy as np
 import pandas as pd
@@ -27,11 +27,6 @@ def process_image(folders):
     # results_folder = '/home/jullygh/sssilvar/Documents/results_same_centroid'
     # mni_file = join(root, 'param', 'FSL_MNI152_FreeSurferConformed_1mm.nii')
 
-    # Get template centroid
-    mni_aseg = nb.load(mni_file).get_data()
-    centroid = tuple(get_centroid(mni_aseg > 0))
-    print('[  OK  ] Centroid = {}'.format(centroid))
-
     # Start processing the whole dataset
     for folder in [folders]:
         # Set of folders important in the processing pipeline
@@ -40,77 +35,81 @@ def process_image(folders):
         brainmask_file = join(subject_dir, 'brainmask_reg.nii.gz')
         subject_output_dir = join(results_folder, folder)
 
-        # Print info message
-        print('[  INFO  ] Processing subject %s located in %s' % (folder, subject_dir))
+        # Execute if file exists
+        if isfile(brainmask_file):
+            # Print info message
+            print('[  INFO  ] Processing subject %s located in %s' % (folder, subject_dir))
 
-        # Try creating a folder for each subject
-        try:
-            os.mkdir(subject_output_dir)
-            print('[  OK  ] Folder created at: ' + subject_output_dir)
-        except OSError:
-            print('[  WARNING  ] Folder already exists.')
+            # Try creating a folder for each subject
+            try:
+                os.mkdir(subject_output_dir)
+                print('[  OK  ] Folder created at: ' + subject_output_dir)
+            except OSError:
+                print('[  WARNING  ] Folder already exists.')
 
-        # Load MRI image and aseg file
-        mgz = nb.load(brainmask_file)
-        img = mgz.get_data().astype(np.float)
+            # Load MRI image and aseg file
+            mgz = nb.load(brainmask_file)
+            img = mgz.get_data().astype(np.float)
 
-        # Calculate the gradient: img_grad
-        gx, gy, gz = np.gradient(img)
-        img_grad = np.sqrt(gx ** 2 + gy ** 2 + gz ** 2)
+            # Calculate the gradient: img_grad
+            gx, gy, gz = np.gradient(img)
+            img_grad = np.sqrt(gx ** 2 + gy ** 2 + gz ** 2)
 
-        # # Load aseg.mgz file: aseg, and centroid calculation: centroid
-        # mgz = nb.load(aseg_file)
-        # aseg = mgz.get_data()
-        # centroid = tuple(get_centroid(aseg > 0))
+            # # Load aseg.mgz file: aseg, and centroid calculation: centroid
+            # mgz = nb.load(aseg_file)
+            # aseg = mgz.get_data()
+            # centroid = tuple(get_centroid(aseg > 0))
 
-        scales = [
-            (0, 25),
-            (25, 50),
-            (50, 75),
-            (75, 100),
-        ]
+            scales = [
+                (0, 25),
+                (25, 50),
+                (50, 75),
+                (75, 100),
+            ]
 
-        for n_scale, scale in enumerate(scales):
-            # Crete a solid angle from a scale: sa
-            r_min, r_max = scale
-            sa = solid_cone(radius=(r_min, r_max), center=centroid)
+            for n_scale, scale in enumerate(scales):
+                # Crete a solid angle from a scale: sa
+                r_min, r_max = scale
+                sa = solid_cone(radius=(r_min, r_max), center=centroid)
 
-            # Start go over the whole sphere (x_angle: [0, pi] and z_angle [-pi, pi])
-            img_2d = np.zeros([360, 180])
-            img_grad_2d = np.zeros_like(img_2d)
+                # Start go over the whole sphere (x_angle: [0, pi] and z_angle [-pi, pi])
+                img_2d = np.zeros([360, 180])
+                img_grad_2d = np.zeros_like(img_2d)
 
-            mask_sub, center = extract_sub_volume(sa, radius=(r_min, r_max), centroid=centroid)
-            vol_sub, _ = extract_sub_volume(img, radius=(r_min, r_max), centroid=centroid)
-            grad_sub, _ = extract_sub_volume(img_grad, radius=(r_min, r_max), centroid=centroid)
+                mask_sub, center = extract_sub_volume(sa, radius=(r_min, r_max), centroid=centroid)
+                vol_sub, _ = extract_sub_volume(img, radius=(r_min, r_max), centroid=centroid)
+                grad_sub, _ = extract_sub_volume(img_grad, radius=(r_min, r_max), centroid=centroid)
 
-            for i, z_angle in enumerate(range(-180, 180)):
-                for j, x_angle in enumerate(range(0, 180)):
-                    solid_ang_mask = rotate_vol(mask_sub, angles=(x_angle, 0, z_angle))
-                    img_masked = vol_sub * solid_ang_mask
-                    grad_masked = grad_sub * solid_ang_mask
+                for i, z_angle in enumerate(range(-180, 180)):
+                    for j, x_angle in enumerate(range(0, 180)):
+                        solid_ang_mask = rotate_vol(mask_sub, angles=(x_angle, 0, z_angle))
+                        img_masked = vol_sub * solid_ang_mask
+                        grad_masked = grad_sub * solid_ang_mask
 
-                    # Number of voxels analyzed
-                    n= solid_ang_mask.sum()
+                        # Number of voxels analyzed
+                        n= solid_ang_mask.sum()
 
-                    # Set pixel in plane as the mean of the voxels analyzed
-                    img_2d[i, j] = np.nan_to_num(img_masked.sum() / n)
-                    img_grad_2d[i, j] = np.nan_to_num(grad_masked.sum() / n)
+                        # Set pixel in plane as the mean of the voxels analyzed
+                        img_2d[i, j] = np.nan_to_num(img_masked.sum() / n)
+                        img_grad_2d[i, j] = np.nan_to_num(grad_masked.sum() / n)
 
-                print('[ SA ] Scale: %d %s Ang: %s | Point (%d, %d) of (360/180) | Value: %f' %
-                      (n_scale + 1, scale, (x_angle, z_angle), i, j, img_2d[i, j]))
+                    print('[ SA ] Scale: %d %s Ang: %s | Point (%d, %d) of (360/180) | Value: %f' %
+                        (n_scale + 1, scale, (x_angle, z_angle), i, j, img_2d[i, j]))
 
-            # Create results:
-            # 2 png files / 2 raw files
+                # Create results:
+                # 2 png files / 2 raw files
 
-            # PNG output for intensities
-            img_filename = join(subject_output_dir, 'intensity_%d_to_%d_solid_angle_to_sphere' % (r_min, r_max))
-            plt.imsave(img_filename + '.png', img_2d, cmap='gray')
-            img_2d.tofile(img_filename + '.raw')
+                # PNG output for intensities
+                img_filename = join(subject_output_dir, 'intensity_%d_to_%d_solid_angle_to_sphere' % (r_min, r_max))
+                plt.imsave(img_filename + '.png', img_2d, cmap='gray')
+                img_2d.tofile(img_filename + '.raw')
 
-            # RAW output for gradients
-            grad_filename = join(subject_output_dir, 'gradient_%d_to_%d_solid_angle_to_sphere' % (r_min, r_max))
-            plt.imsave(grad_filename + '.png', img_grad_2d, cmap='gray')
-            img_grad_2d.tofile(grad_filename + '.raw')
+                # RAW output for gradients
+                grad_filename = join(subject_output_dir, 'gradient_%d_to_%d_solid_angle_to_sphere' % (r_min, r_max))
+                plt.imsave(grad_filename + '.png', img_grad_2d, cmap='gray')
+                img_grad_2d.tofile(grad_filename + '.raw')
+        else:
+            print('[  ERROR  ] File {} was not found'.format(brainmask_file))
 
 
 if __name__ == '__main__':
@@ -129,8 +128,13 @@ if __name__ == '__main__':
 
     dataset_registered_folder = config.get('dirs', 'dataset_folder_registered')
     results_folder = config.get('dirs', 'sphere_mapping')
-    n_cores = config.get('resources', 'n_cores')
+    n_cores = config.getint('resources', 'n_cores')
     mni_file = join(root, 'param', 'FSL_MNI152_FreeSurferConformed_1mm.nii')
+
+    # Get template centroid
+    mni_aseg = nb.load(mni_file).get_data()
+    centroid = tuple(get_centroid(mni_aseg > 0))
+    print('[  OK  ] Centroid = {}'.format(centroid))
 
     # Pool the process
     pool = Pool(n_cores)
