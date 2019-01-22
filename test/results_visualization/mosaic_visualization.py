2bd7d096896cb898f8b41dfd6a71b1f50a0d5deb
Santiago Smith <sssilvar@unal.edu.co>
2019-01-21 21:39:54 -0500

    Mosaic add ups

--- test/results_visualization/mosaic_visualization.py
+++ test/results_visualization/mosaic_visualization.py
@@ -9,7 +9,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 
-plt.style.use('ggplot')
+# plt.style.use('ggplot')
 
 
 def imread(filename):
@@ -49,8 +49,9 @@ if __name__ == "__main__":
                 im_path = join(root, f)
                 images.append(im_path)
     
+    # images = images[:30]
     # Plot images
-    n_cols = 5
+    n_cols = 6
     n_rows = int(np.ceil(len(images) / n_cols))
     fig, axes = plt.subplots(ncols=n_cols, nrows=n_rows)
     # fig.suptitle('Sphere mapped visualization ({} images)'.format(im_type))
@@ -67,7 +68,9 @@ if __name__ == "__main__":
                     # Set title
                     sid = basename(dirname(img_file))
                     tg, age, sex = get_dx_age_and_size(df)
-                    ax.set_title('{} | {} yrs | {}'.format(tg, age, sex), color='r' if tg == 'MCIc' else 'b')
+                    # ax.set_title('{} | {} yrs | {}'.format(tg, age, sex), color='r' if tg == 'MCIc' else 'b')
+                    ax.set_title('{} '.format(tg), color='r' if tg == 'MCIc' else 'b')
+
 
                 except IndexError as e:
                     print('[ WARNING  ] {}'.format(e))
@@ -89,4 +92,33 @@ if __name__ == "__main__":
             
             # Extra config
             axs.axis('off')
+    # plt.savefig('/tmp/plot.pdf', bbox_inches='tight', orientation='landscape', papertype='a0')
+    # plt.show()
+
+    # Mean absolute difference
+    mcic_img = np.zeros_like(img)
+    mcinc_img = np.zeros_like(mcic_img)
+    mcic_count = 0
+    mcinc_count = 0
+
+    for img_file in images:
+        sid = basename(dirname(img_file))
+        img = imread(img_file)
+        
+        dx = df.loc[sid, 'target']
+        if dx == 'MCIc':
+            mcic_img += img
+            mcic_count += 1
+        elif dx == 'MCInc':
+            mcinc_img += img
+            mcinc_count += 1
+    
+    # Do average
+    mcic_img /= mcic_count
+    mcinc_img /= mcinc_count
+    
+    fig, ax = plt.subplots(ncols=3, nrows=1)
+    ax[0].imshow(mcic_img)
+    ax[1].imshow(mcinc_img)
+    ax[2].imshow(np.abs(mcic_img - mcinc_img))
     plt.show()
