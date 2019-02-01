20003371586aa18881f8d83179de6718f17c743d
Santiago Smith <sssilvar@unal.edu.co>
2019-02-01 05:25:21 -0500

    Some warnings solved

--- py/01_preprocessing/02_rigid_registration_with_flirt.py
+++ py/01_preprocessing/02_rigid_registration_with_flirt.py
@@ -14,11 +14,13 @@ print('[  ROOT  ] {}'.format(root))
 
 sys.path.append(root)
 
+
 @contextmanager
 def poolcontext(*args, **kwargs):
     pool = Pool(*args, **kwargs)
     yield pool
     pool.terminate()
+
 
 def register_subject_with_flirt(subject_id):
     """
@@ -26,7 +28,7 @@ def register_subject_with_flirt(subject_id):
     """
     # Define template volume directory
     dst = os.path.join(root, 'param', 'FSL_MNI152_FreeSurferConformed_1mm.nii')
-    
+
     # Check if file is zipped
     subjects_dir = cfg.get('dirs', 'subjects_dir')
     mov = os.path.join(subjects_dir, subject_id, 'mri', 'brainmask.mgz')
@@ -34,20 +36,19 @@ def register_subject_with_flirt(subject_id):
 
     if not isfile(mov) and isfile(zipped_file):
         print('[  INFO  ] Extracting data from: {}'.format(zipped_file))
-        bmask_path =join(subject_id, 'mri/brainmask.mgz')
+        bmask_path = join(subject_id, 'mri/brainmask.mgz')
 
         with ZipFile(zipped_file, 'r') as zf:
             zf.extract(bmask_path, '/dev/shm/')
         subjects_dir = '/dev/shm'
-    
-    
+
     # Define mooving volumes (We have to convert MGZ to NIFTI due to FSL compatibility)
     mov = os.path.join(subjects_dir, subject_id, 'mri', 'brainmask.mgz')
     mov_nii = '/dev/shm/{}.nii.gz'.format(subject_id)
     aff_mat = os.path.join(registered_folder, subject_id, 'transform.mat')
     mapmov = os.path.join(registered_folder, subject_id, 'brainmask_reg.nii.gz')
     print(mov)
-    
+
     # Check if file exists
     if isfile(mov):
         # Create a folder per each subject
@@ -64,8 +65,9 @@ def register_subject_with_flirt(subject_id):
         if isdir('/dev/shm/{}'.format(subject_id)):
             os.system('rm -rf /dev/shm/{}'.format(subject_id))
 
-        # Register to MNI
-        # flirt -ref /home/sssilvar/Documents/code/CSD-AD/param/FSL_MNI152_FreeSurferConformed_1mm.nii -in brain.nii.gz -omat my_affine_transf.mat -out registered.nii.gz
+        # Register to MNI flirt
+        # -ref FSL_MNI152_FreeSurferConformed_1mm.nii -in brain.nii.gz
+        # -omat my_affine_transf.mat -out registered.nii.gz
         command = 'flirt -ref {} -in {} -omat {} -out {}'.format(dst, mov_nii, aff_mat, mapmov)
         os.system(command)
 
@@ -73,7 +75,6 @@ def register_subject_with_flirt(subject_id):
         os.system('rm {}'.format(mov_nii))
     else:
         print('[  ERROR  ] File {} not found'.format(mov))
-
 
 
 if __name__ == '__main__':
@@ -88,10 +89,9 @@ if __name__ == '__main__':
     dataset_folder = cfg.get('dirs', 'dataset_folder')
     registered_folder = cfg.get('dirs', 'dataset_folder_registered')
     n_cores = cfg.getint('resources', 'n_cores')
-    
+
     # Read file containing subject's IDs
     df = pd.read_csv(join(dataset_folder, 'groupfile.csv'), index_col=0)
 
     with poolcontext(processes=n_cores) as pool:
         pool.map(register_subject_with_flirt, df.index)
-    
