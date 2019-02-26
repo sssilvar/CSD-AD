9430c5bbe0a55308d77477c7e5b7000ae53955e6
Santiago Smith <sssilvar@unal.edu.co>
2019-02-25 22:37:45 -0500

    Some Changes

---
+++ test/explore_data/visualize_data_stats.py
@@ -0,0 +1,17 @@
+from os.path import join, realpath, dirname
+
+import pandas as pd
+
+root = dirname(dirname(dirname(realpath(__file__))))
+
+if __name__ == '__main__':
+    # Load conversions file
+    conversions_file = join(root, 'param/df_conversions_with_times.csv')
+    df = pd.read_csv(conversions_file, index_col='PTID')
+
+    # Set a conversion times of interes: toi
+    toi = [24, 48, 60]
+    for t in toi:
+        df_mci = df.loc[(df['Month.CONVERSION'] <= t) | (df['Month.STABLE'] >= t)]
+        print('Data distribution for {} months'.format(t))
+        print(df_mci['target'].value_counts())
