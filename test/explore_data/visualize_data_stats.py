ef706354f1730ea2f6de88e37d085d1cabe982ae
Santiago Smith <sssilvar@unal.edu.co>
2019-03-06 17:16:45 -0500

    Some Changes

--- test/explore_data/visualize_data_stats.py
+++ test/explore_data/visualize_data_stats.py
@@ -8,6 +8,9 @@ if __name__ == '__main__':
     # Load conversions file
     conversions_file = join(root, 'param/df_conversions_with_times.csv')
     df = pd.read_csv(conversions_file, index_col='PTID')
+    print('\nGlobal stats:')
+    print(df['target'].value_counts())
+    print()
 
     # Set a conversion times of interes: toi
     toi = [24, 36, 60]
