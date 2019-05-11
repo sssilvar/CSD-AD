3e52e3735665c89daeee0a8e98d6164d241753ff
Santiago Smith <sssilvar@unal.edu.co>
2019-05-11 17:50:59 -0500

    Some changes

---
+++ py/04_mapping_sphere/sphere_mapping_indexes_sql.py
@@ -0,0 +1,87 @@
+#!/bin/env python3
+import os
+import sys
+from os.path import join, dirname, realpath
+
+import numpy as np
+import pandas as pd
+import nibabel as nb
+from scipy import ndimage as ndi
+
+from sqlalchemy import create_engine
+
+from nilearn import plotting
+import matplotlib.pyplot as plt
+
+# Set root folder and append it to path
+root = dirname(dirname(dirname(realpath(__file__))))
+sys.path.append(root)
+
+from lib.masks import solid_cone
+from lib.transformations import rotate_vol
+from lib.geometry import extract_sub_volume, get_centroid
+
+if __name__ == '__main__':
+    # Set parameters
+    print(10 * '=' + ' Index saver ' + 10 * '=')
+    data_folder = join(os.getenv('HOME'), 'Downloads')
+    mni_file = join(root, 'param', 'FSL_MNI152_FreeSurferConformed_1mm.nii')
+    engine = create_engine('sqlite:////dev/shm/indexes.sqlite')
+
+    tk = 25
+    overlap = 9
+    max_radius = 100
+
+    ns = 2  # TODO: Check if it's necessary to change it (Scaling factor
+    df_out_file = join(data_folder, 'indexes_tk_{}_overlap_{}_ns_{}.h5'.format(tk, overlap, ns))
+
+    # Print some info
+    print('\t- Output folder: %s' % data_folder)
+    print('\t- Output file: %s' % df_out_file)
+
+    # Calculate the inner and outer radius
+    # for all the spheres: scales
+    n_spheres = max_radius // (tk - overlap)
+    scales = [(i * (tk - overlap), ((i + 1) * tk) - (i * overlap)) for i in range(n_spheres)]
+    print('Number of scales: {} | Scales: {}'.format(len(scales), scales))
+
+    # Get centroid of MNI152
+    mni_aseg = nb.load(mni_file)
+    centroid = tuple(get_centroid(mni_aseg.get_data() > 0))
+    print('Centroid of MNI152: {}'.format(centroid))
+
+    spheres = np.zeros(mni_aseg.shape)
+    for i, (r_min, r_max) in enumerate(scales):
+        print('Creating sphere: {} ...'.format(i + 1))
+        sc = solid_cone(radius=(r_min, r_max), center=centroid)
+        spheres[np.where(sc)] = i + 1
+
+    # ==== INDEX CALCULATION ====
+    for i, z_angle in enumerate(range(-180, 180, ns)):
+        for j, x_angle in enumerate(range(0, 180, ns)):
+            print('Processing angles: ({}, {})'.format(z_angle, x_angle))
+            solid_ang_mask = rotate_vol(spheres, angles=(x_angle, 0, z_angle))  # ROI
+            for k, (r_min, r_max) in enumerate(scales):
+                scale = '{}_{}'.format(r_min, r_max)
+                ix = np.where(solid_ang_mask == k + 1)
+                data = {
+                    'scale': scale,
+                    'theta': z_angle,
+                    'phi': x_angle,
+                    'ix': ix[0].tostring(),
+                    'iy': ix[1].tostring(),
+                    'iz': ix[2].tostring(),
+                }
+                s = pd.DataFrame()
+                s = s.append(pd.Series(data), ignore_index=True)
+                s.to_sql('indexes', con=engine, if_exists='append')
+                print(s)
+            # Plot for (180, 0) degrees
+            if z_angle == -180 and x_angle == 0:
+                nii_a = nb.Nifti1Image(solid_ang_mask, mni_aseg.affine)
+                nb.save(nii_a, '/tmp/cones.nii')
+
+                display = plotting.plot_anat(mni_aseg)
+                display.add_overlay(nii_a)
+                plt.show()
+    print('DONE!')
--- py/05_sparse_representation/create_features_matrix.py
+++ py/05_sparse_representation/create_features_matrix.py
@@ -1,25 +1,37 @@
 #!/bin/env python2
+from __future__ import print_function
+
 import os
+import sys
 import glob
-from os.path import join, realpath, dirname, basename, splitext
+from os.path import join, realpath, dirname, basename, splitext, isdir
 from configparser import ConfigParser
 
 import pyct as ct
 import numpy as np
 import pandas as pd
+from scipy.stats import describe, gennorm
 
 # Set root folder
 root = dirname(dirname(dirname(realpath(__file__))))
+
+sys.path.append(join(root))
+from lib.curvelets import get_sub_bands
 
 if __name__ == '__main__':
     # Load configuration file
     cfg_file = join(root, 'config', 'config.cfg')
     cfg = ConfigParser()
     cfg.read(cfg_file)
+    nbs = 4
+    nba = 32
 
     # Get subjects folder
     mapped_subjects_dir = cfg.get('dirs', 'sphere_mapping')
+    out_folder = join(mapped_subjects_dir, 'curvelet')
+    output_file = join(out_folder, 'sobel_curvelet_features_non_split.csv')
     print('\t- Mapped subjects folder: {}'.format(mapped_subjects_dir))
+    print('\t- Output folder: {}'.format(out_folder))
 
     # Find NPZ files
     raw_files = glob.glob(join(mapped_subjects_dir, '**', '*.npz'))#, recursive=True)
@@ -27,17 +39,44 @@ if __name__ == '__main__':
     # Create features matrix
     features = pd.DataFrame()
 
-    for f in raw_files[:10]:
+    for f in raw_files:
         subject_id = f.split('/')[-2]
         file_basename = splitext(basename(f))[0].split('_')
-        scale = '_'.join(file_basename[-2:])
+        scale = '_to_'.join(file_basename[-2:])
         img_type = file_basename[0]
         print(f, scale, subject_id)
-
-        features = features.append({'sid': subject_id, 'type': img_type, 'scale': scale}, ignore_index=True)
 
         # Load image
         img = np.load(f)['img']
 
+        # Curvelet analysis
+        A = ct.fdct2(
+            img.shape,
+            nbs=nbs,
+            nba=nba,
+            ac=True,
+            norm=False,
+            vec=True,
+            cpx=False)
+
+        feats = pd.Series()
+        feats.name = subject_id
+        feats['sphere'] = scale
+        f = A.fwd(img)  # Forward transformation
+        data_dict = get_sub_bands(A, f)  # Transform into a dictionary
+        for key, val in data_dict.items():
+            beta_est, mean_est, var_est = gennorm.fit(val)
+            feats[key + '_beta'] = beta_est
+            feats[key + '_mean'] = mean_est
+            feats[key + '_var'] = var_est
+
+        features = features.append(feats)
+
+    # Create folder if does not exist
+    if not isdir(out_folder):
+        os.mkdir(out_folder)
+
     print(features.head())
+    features.to_csv(output_file)
+    print('Done!')
 
