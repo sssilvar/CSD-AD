4b4fcce1ed228f4cc09a90771685ee0a846beab2
Santiago Smith <sssilvar@unal.edu.co>
2019-05-13 16:59:58 -0500

    Some changes

--- py/04_mapping_sphere/map_from_indexes_sql.py
+++ py/04_mapping_sphere/map_from_indexes_sql.py
@@ -30,6 +30,13 @@ def sobel_magnitude(nii_file):
     return np.sqrt(sobel_x ** 2 + sobel_y ** 2 + sobel_z ** 2)
 
 
+def grad_magnitude(nii_file):
+    # Load file
+    vol = nb.load(nii_file).get_data().astype(np.float)
+    gx, gy, gz = np.gradient(vol)
+    return np.sqrt(gx ** 2 + gy ** 2 + gz ** 2)
+
+
 def subject_mapping(subject):
     print(f'Processing subject {subject}')
     subject_out_dir = join(out_folder, subject)
@@ -38,9 +45,11 @@ def subject_mapping(subject):
 
     # Load volume
     vol_sobel = sobel_magnitude(join(subject_dir, subject_file))
+    vol_grad = grad_magnitude(join(subject_dir, subject_file))
     for scale in scales:
         scale_name = '%d_%d' % scale
-        img = np.zeros([360 // ns, 180 // ns])
+        img_sobel = np.zeros([360 // ns, 180 // ns])
+        img_grad = np.zeros_like(img_sobel)
 
         # Create dataframe per scale
         res_sql = engine.execute(f'SELECT * FROM indexes WHERE scale=\'{scale_name}\'')
@@ -56,11 +65,13 @@ def subject_mapping(subject):
                 ix_where = np.frombuffer(ix, dtype=int), np.frombuffer(iy, dtype=int), np.frombuffer(iz, dtype=int)
 
                 # Map result
-                img[i, j] = vol_sobel[ix_where].mean()
+                img_sobel[i, j] = vol_sobel[ix_where].mean()
+                img_grad[i, j] = vol_grad[ix_where].mean()
         # Save mapped images
         file_basename = join(subject_out_dir, f'{scale_name}')
-        plt.imsave(file_basename + '.png', img, cmap='gray')
-        np.savez_compressed(file_basename, img=img, indexes=sql_file, brain_file=subject_file)
+        plt.imsave(file_basename + '_sobel.png', img_sobel, cmap='gray')
+        plt.imsave(file_basename + '_grad.png', img_grad, cmap='gray')
+        np.savez_compressed(file_basename, img=img_sobel, grad=img_grad, indexes=sql_file, brain_file=subject_file)
 
 
 if __name__ == '__main__':
@@ -98,5 +109,3 @@ if __name__ == '__main__':
     subjects = pd.read_csv(group_file, index_col=0)
     pool = Pool(n_cores)
     pool.map(subject_mapping, subjects.index)
-
-
