05da9dd4d95cf34be8943e53ace6d9f16a541d0f
Santiago Smith <sssilvar@unal.edu.co>
2019-05-19 20:40:20 -0500

    Some changes

--- py/05_sparse_representation/classify_all.sh
+++ py/05_sparse_representation/classify_all.sh
@@ -7,7 +7,9 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TIMES=(24 36 60)
 
 # Features file
-FEATS="/home/jullygh/sssilvar/Documents/Dataset/ADNI_FS_sphere_mapped_6_sph_overlap/curvelets_non_split"
+FEATS=${1-"/home/jullygh/sssilvar/Documents/Dataset/ADNI_FS_sphere_mapped_6_sph_overlap/curvelets_non_split"}
+NBS=${2-"4"}
+NBA=${3-"32"}
 
 # Set other params
 CLASSIFIERS=("svm" "rf")
@@ -21,7 +23,8 @@ do
         for t in ${TIMES[@]}
         do
             echo "Processing ${imtype} images for subjects in ${t} months of conversion/stability..."
-            SCRIPT="${CURRENT_DIR}/splitting_images/classify_complete.py -time ${t} -folds ${FOLDS} -clf ${clf} -imtype ${imtype} -tune 0 -features ${FEATS}/${imtype}_curvelet_features_non_split.csv"
+            FEATS_FILE="${FEATS}/${imtype}_curvelet_features_non_split_${NBS}_scales_${NBA}_angles.csv"
+            SCRIPT="${CURRENT_DIR}/splitting_images/classify_complete.py -time ${t} -folds ${FOLDS} -clf ${clf} -imtype ${imtype} -tune 0 -features ${FEATS_FILE}"
             eval "python3 ${SCRIPT}"
 
             echo -e "\n\n"
