703949eda4ff4ffa8615f649bd5b13a71f69fb13
Santiago <sssilvar@unal.edu.co>
2019-05-31 06:46:59 -0700

    Extra cases for index generation

--- bash/curvelet_features.sh
+++ bash/curvelet_features.sh
@@ -5,10 +5,10 @@ SCRIPT="${CURRENT_DIR}/../py/05_sparse_representation/create_features_matrix.py"
 NOTIFIER="${CURRENT_DIR}/../lib/email_notifier.py -subject 'Index calculation finished'"
 MAPPED_FOLDER="/home/jullygh/sssilvar/Documents/Dataset/mapped"
 
-FOLDERS=($(ls ${MAPPED_FOLDER} | grep overlap))
+FOLDERS=($(find ${MAPPED_FOLDER} -type d -name "ADNI*tk*overlap*"))
 
 for folder in ${FOLDERS[@]} ; do
-    if [[ -d "${folder}/curvelet" ]]; then
+    if [[ ! -d "${folder}/curvelet" ]]; then
         echo "PRocessing $folder"
     fi
 done
