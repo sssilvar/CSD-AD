502f7c7c9de642458f17b750d6cd7f2c68a6075c
Santiago Smith <sssilvar@unal.edu.co>
2019-06-18 07:58:40 -0500

    Using the norm instead of the generalized gaussian

--- bash/curvelet_features_norm.sh
+++ bash/curvelet_features_norm.sh
@@ -9,20 +9,18 @@ N_SUBJ=829
 FOLDERS=($(find ${MAPPED_FOLDER} -type d -name "ADNI*tk*overlap*"))
 
 for folder in ${FOLDERS[@]} ; do
-    if [[ ! -d "${folder}/curvelet" ]]; then
-        n=$(ls ${folder} | grep _S_ | wc -l)
-        if [[ "${n}" == "${N_SUBJ}" ]]; then
-            echo "Processing ${folder} (${n} subjects)"
-            tk=$(echo basename ${folder} | awk -F'_' '{ print $5 }')
-            overlap=$(echo basename ${folder} | awk -F'_' '{ print $7 }')
-            ns=$(echo basename ${folder} | awk -F'_' '{ print $9 }')
+    n=$(ls ${folder} | grep _S_ | wc -l)
+    if [[ "${n}" == "${N_SUBJ}" ]]; then
+        echo "Processing ${folder} (${n} subjects)"
+        tk=$(echo basename ${folder} | awk -F'_' '{ print $5 }')
+        overlap=$(echo basename ${folder} | awk -F'_' '{ print $7 }')
+        ns=$(echo basename ${folder} | awk -F'_' '{ print $9 }')
 
-            CMD="${SCRIPT} -tk ${tk} -overlap ${overlap} -ns ${ns}"
-            CMD="${CMD} && ${NOTIFIER} -msg 'Curvelet calculation for tk=${tk} and overlap=${overlap} done.'"
-            CMD="tmux new-session -d -s \"curvelet_tk_${tk}_ov_${overlap}\" \"${CMD}\""
+        CMD="${SCRIPT} -tk ${tk} -overlap ${overlap} -ns ${ns}"
+        CMD="${CMD} && ${NOTIFIER} -msg 'Curvelet calculation for tk=${tk} and overlap=${overlap} done.'"
+        CMD="tmux new-session -d -s \"curvelet_tk_${tk}_ov_${overlap}\" \"${CMD}\""
 
-            echo ${CMD}
-            eval ${CMD}
-        fi
+        echo ${CMD}
+        eval ${CMD}
     fi
 done
