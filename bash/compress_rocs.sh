f4ad2e4475a9bf2d62ac6bf5bc187bbf8536920b
Santiago Smith <sssilvar@unal.edu.co>
2019-06-22 09:13:16 -0500

    KBest added to feature selection

--- bash/compress_rocs.sh
+++ bash/compress_rocs.sh
@@ -10,13 +10,15 @@ N_SUBJ=829
 FOLDERS=($(find ${MAPPED_FOLDER} -type d -name "ADNI*tk*overlap*"))
 
 for folder in ${FOLDERS[@]} ; do
-    curv_folder="${folder}/curvelet/"
+    curv_folder="${folder}/curvelet"
     count_files=`ls -1 ${curv_folder}*.csv 2>/dev/null | wc -l`
     if [[ ${count_files} > 0 ]]; then
         n=$(ls ${folder} | grep _S_ | wc -l)
         if [[ "${n}" == "${N_SUBJ}" ]]; then
             if [[ -d "${curv_folder}/ROC" ]]; then
-                echo "$curv_folder/roc"
+                CMD="tar -rvz ~/Downloads/ROCS.tar.gz ${curv_folder}/ROC"
+                echo ${CMD}
+                eval ${CMD}
             fi
         fi
     fi
