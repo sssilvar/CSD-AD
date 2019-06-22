b2e2637a5fbf0d640ed4c0704a041c95c8cbaa94
Santiago Smith <sssilvar@unal.edu.co>
2019-06-22 09:27:59 -0500

    KBest added to feature selection

--- py/05_sparse_representation/splitting_images/classify_complete.py
+++ py/05_sparse_representation/splitting_images/classify_complete.py
@@ -16,7 +16,7 @@ from sklearn.ensemble import RandomForestClassifier
 
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelBinarizer, StandardScaler
-from sklearn.feature_selection import SelectFromModel, SelectKBest
+from sklearn.feature_selection import SelectFromModel, SelectKBest, mutual_info_classif
 from sklearn.model_selection import GridSearchCV, StratifiedKFold, train_test_split, KFold
 from sklearn.metrics import classification_report, roc_auc_score, roc_curve, accuracy_score, confusion_matrix
 
@@ -276,7 +276,7 @@ if __name__ == "__main__":
         pipeline = Pipeline([
             ('scaler', StandardScaler()),
             # ('feature_selection', SelectFromModel(LinearSVC(penalty='l2'))),
-            ('feature_selection', SelectKBest(k=100)),
+            ('feature_selection', SelectKBest(mutual_info_classif)),
             ('clf', clf)
         ])
 
